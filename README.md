81f9f3c8963ce6d2302694be8114b2250557c948
Evan Philip <107-ephilip@users.noreply.gitlab.pasqal.io>
2022-11-08 16:44:13 +0100

    Updates after successful usage

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Match Displays
 
-Match color and brightness of two display monitors using your phone camera. 
+Match color and brightness of two display monitors using your phone camera.
 
 ## Why?
-I am not an artist, but the different tints on my two monitors was nagging me. I did not want to create color profiles, I just wanted to get the monitors to display similar colors using the hardware adjustment on the montor itself. I thought a modern phone camera might be more accurate than eyeballing.  
+
+I am not an artist, but the different tints on my two monitors was nagging me. I did not want to create color profiles, I just wanted to get the monitors to display similar colors using the hardware adjustment on the montor itself. I thought a modern phone camera might be more accurate than eyeballing.
+
+I used a second-generation iPhone SE with Reincubate Camo. The result was much better than anything I could manage without it.
 
 ## Usage
+
 This is a simple script that displays the RGB values of two spots in an IP video stream. After setting up an IP video stream (this has to be done separately), the display monitors and camera can be arranged such that the two spots fall on points on the two displays that are supposed to be identical. Now the displays can be adjusted using their hardware controls.
--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import curses
 import cv2
 import numpy
 
-stream = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+# any stream that cv2.VideoCapture can open
+stream = 0  # "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
 
-size = 3
+# size of the color pickers
+size = 50
 
+# open video stream and get geometry
 cap = cv2.VideoCapture(stream)
 height, width, _ = (cap.read()[1]).shape
+
+# color pickers
 picker1_index = (
     slice((height // 2 - size), (height // 2 + size)),
     slice((1 * width // 3 - size), (1 * width // 3 + size)),
@@ -18,33 +23,38 @@ picker2_index = (
     slice((2 * width // 3 - size), (2 * width // 3 + size)),
 )
 
+# misc initializations
 screen = curses.initscr()
-numpy.set_printoptions(formatter={"float_kind": "{0:.2f}".format})
+numpy.set_printoptions(formatter={"float_kind": "{0:.3f}".format})
+
+# loop through video frames
 while cap.isOpened():
     img = cap.read()[1]
+
+    # read and process the color patches
     patch1 = img[picker1_index]
     patch2 = img[picker2_index]
     total1 = numpy.average(patch1)
     total2 = numpy.average(patch2)
-    bgr1 = numpy.flip(numpy.average(patch1, axis=(0, 1)))
-    bgr2 = numpy.flip(numpy.average(patch2, axis=(0, 1)))
+    rgb1 = numpy.flip(numpy.average(patch1, axis=(0, 1)))
+    rgb2 = numpy.flip(numpy.average(patch2, axis=(0, 1)))
 
     # print color
     screen.clear()
-    screen.addstr(0, 1, "R    G    B")
-    screen.addstr(0, 18, "Total")
+    screen.addstr(0, 1, "R     G     B")
+    screen.addstr(0, 21, "Total")
     screen.addstr(
         1,
         0,
-        f"{bgr1 / sum(bgr1)}",
+        f"{rgb1 / sum(rgb1)}",
     )
-    screen.addstr(1, 18, f"{total1 / 255:.3f}")
+    screen.addstr(1, 21, f"{total1 / 255:.3f}")
     screen.addstr(
         2,
         0,
-        f"{bgr2 / sum(bgr2)}",
+        f"{rgb2 / sum(rgb2)}",
     )
-    screen.addstr(2, 18, f"{total2 / 255:.3f}")
+    screen.addstr(2, 21, f"{total2 / 255:.3f}")
     screen.addstr(4, 0, "Press 'q' to quit.")
     screen.refresh()
 
@@ -52,6 +62,7 @@ while cap.isOpened():
     img[picker1_index] = 255 - img[picker1_index]
     img[picker2_index] = 255 - img[picker2_index]
 
+    # display frame until 'q' is pressed
     cv2.imshow("Frame", numpy.array(img, dtype=numpy.uint8))
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
